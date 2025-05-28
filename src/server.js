1e88e88890126e7685c39e6b52f81f8567076608
Anastasiia <anastasiiprfnk@gmail.com>
2025-05-28 21:18:17 +0300

    add message

--- src/server.js
+++ src/server.js
@@ -26,6 +26,8 @@ export const setupServer = async () => {
     app.get('/contacts', async(req, res) => {
         const contacts = await getAllContacts();
         res.status(200).json({
+            status: 200,
+            message: "Successfully found contacts!",
             data: contacts,
         })
     });
@@ -41,6 +43,8 @@ export const setupServer = async () => {
         }
 
         res.status(200).json({
+          status: 200,
+          message: `Successfully found contact with id ${contactId}!`,
           data: contact,
         });
 
