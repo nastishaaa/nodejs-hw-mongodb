81f27404545f0186c4b9c6f2d504a47eeb062a11
Anastasiia <anastasiiprfnk@gmail.com>
2025-06-01 21:07:29 +0300

    import

--- src/utils/parseSortParams.js
+++ src/utils/parseSortParams.js
@@ -1,4 +1,5 @@
-import { SORT_ORDER } from ".."
+import { SORT_ORDER } from "../index";
+
 const parseSortOrder = (sortOrder) => {
     const isKnownOrder = [SORT_ORDER.ASC, SORT_ORDER.DESC].includes(sortOrder);
     if (isKnownOrder) return sortOrder;
@@ -9,10 +10,10 @@ const parseSortOrder = (sortOrder) => {
     const keysOfStudent = [
       '_id',
       'name',
-      'age',
-      'gender',
-      'avgMark',
-      'onDuty',
+      'phoneNumber',
+      'email',
+      'isFavourite',
+      'contactType',
       'createdAt',
       'updatedAt',
     ];
