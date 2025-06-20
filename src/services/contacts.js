beb7a4a6b1589852e3265349ffac5f6b1a1f4d94
Anastasiia <anastasiiprfnk@gmail.com>
2025-06-20 23:26:21 +0300

    contact id changed

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -38,8 +38,8 @@ export const getAllContacts = async ({ page, perPage,
     };
 };
 
-export const getContactById = async (contactId, userId) => {
-    const contact = await Contact.findOne({ _id: contactId, userId });
+export const getContactById = async (contactId) => {
+    const contact = await Contact.findOne({ _id: contactId });
     return contact;
 }
 
