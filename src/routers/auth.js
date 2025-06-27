f27071bd8932b247309440653daf1c2a649a5f84
Anastasiia <anastasiiprfnk@gmail.com>
2025-06-27 20:50:12 +0300

    add resetController

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
-import { registerUserController, loginUserController, refreshUserController, logoutUserController, sendResetEmailUserController } from "../controllers/auth.js";
+import { registerUserController, loginUserController, refreshUserController, logoutUserController, sendResetEmailUserController, resetUserPasswordController } from "../controllers/auth.js";
 import { registerUserSchema, loginSchema, sendResetPasswordSchema, resetPasswordSchema } from "../validation/user.js";
 import { validateBody } from "../middlewares/validateBody.js";
 
@@ -11,6 +11,6 @@ registerRouter.post('/login', validateBody(loginSchema), ctrlWrapper(loginUserCo
 registerRouter.post('/refresh', ctrlWrapper(refreshUserController));
 registerRouter.post('/logout', ctrlWrapper(logoutUserController));
 registerRouter.post('/send-reset-email', validateBody(sendResetPasswordSchema) , ctrlWrapper(sendResetEmailUserController));
-registerRouter.post('/reset-pwd', validateBody(resetPasswordSchema) ,ctrlWrapper())
+registerRouter.post('/reset-pwd', validateBody(resetPasswordSchema), ctrlWrapper(resetUserPasswordController));
 
 export default registerRouter;
